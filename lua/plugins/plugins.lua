4a98bbd927c04af8289186a5690f4bf7846b2453
Jonathan Boeckel <jboeckel@mailbox.org>
2024-12-18 19:57:35 +0000

    Add plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,5 +1,8 @@
 return {
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.dial" },
+  { import = "lazyvim.plugins.extras.editor.inc-rename" },
+  { import = "lazyvim.plugins.extras.editor.navic" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.util.rest" },
 
